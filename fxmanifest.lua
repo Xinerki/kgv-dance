2eb43e86bedf6b68249aa75712040e70c7954d80
Xinerki <simasadves@gmail.com>
2023-05-14 20:09:51 +0200

    fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,7 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+game 'gta5'
 
+author 'Xinerki'
 description 'dances from afterhours dlc'
 
 client_script 'client.lua'
